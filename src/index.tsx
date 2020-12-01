0238947773aadb65d9485d803c4051ab7622c6b5
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2020-12-02 00:27:29 +0700

    feat: ok

--- src/index.tsx
+++ src/index.tsx
@@ -1,87 +1,248 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 
-import './index.scss';
-import { Place, Todo } from './types';
-import { isTypeNode } from 'typescript';
+import './base.scss';
+import { Todo } from './types';
+import asset_spinner from './spinner.svg';
+
+const Wrapper = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
+  width: 100vw;
+  min-height: 100vh;
+  padding-top: 60px;
+  background-color: #f5f6ee;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InputWrapper = styled.div`
+  width: 276px;
+  height: 45px;
+  position: relative;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 2px solid #000000;
+  width: 100%;
+  height: 100%;
+  background-color: #f5f6ee;
+  text-align: center;
+  font-size: 14px;
+
+  &::placeholder {
+    color: #c4c4c4;
+  }
+
+  &:hover::placeholder {
+    color: #979797;
+  }
+
+  &:focus {
+    outline: none;
+    &::placeholder {
+      color: #979797;
+    }
+  }
+`;
+
+const TodoList = styled.div`
+  width: 276px;
+  margin-top: 44px;
+`;
+
+const Checkbox = styled.input`
+  &[type='checkbox'] {
+    width: 20px;
+    height: 20px;
+    border-radius: 5px;
+    outline: none;
+    background-color: white;
+    border: 0.2px solid #979797;
+    appearance: none;
+    position: relative;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:checked:after {
+      content: '✓';
+      position: absolute;
+      top: -3.5px;
+      left: 3px;
+    }
+  }
+`;
+
+const Description = styled.span`
+  font-size: 14px;
+  margin-left: 10px;
+  line-height: 16.41px;
+  margin-top: 2px;
+  width: 224px;
+  /* background-color: red; */
+`;
+
+const DeleteButton = styled.button`
+  /* background: yellow; */
+  margin-left: 5px;
+  height: 20px;
+  margin-top: -2px;
+
+  &:after {
+    color: #979797;
+    content: '✕';
+    visibility: hidden;
+  }
+
+  &:hover:after {
+    color: #000000;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Item = styled.div`
+  width: 100%;
+  margin-bottom: 12px;
+  display: flex;
+  border-radius: 5px;
+  /* background-color: lime; */
+
+  &:hover {
+    background-color: #e4e7d5;
+    ${DeleteButton}:after {
+      visibility: visible;
+    }
+  }
+`;
+
+const SpinnerTodoList = styled.img`
+  text-align: center;
+  height: 20px;
+`;
+
+const SpinnerItem = styled.img`
+  height: 15px;
+  margin-top: 3px;
+  margin-left: 4px;
+`;
+
+const SpinnerInput = styled.img`
+  position: absolute;
+  height: 15px;
+  right: 0;
+  top: 14px;
+`;
+
+const SpinnerCheckbox = styled(Checkbox)`
+  &[type='checkbox'] {
+    &:checked:after {
+      content: url(${asset_spinner});
+      transform: scale(0.063);
+      transform-origin: left top;
+      margin-top: 6px;
+      margin-left: 0.5px;
+    }
+  }
+`;
 
 function App() {
   const [list, setList] = React.useState<Todo[]>([]);
-  const [title, setTitle] = React.useState<string>('');
-  const [place, setPlace] = React.useState<Place>();
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [title, setTitle] = React.useState('');
+  const [isLoadingTodoList, setIsLoadingTodoList] = React.useState(false);
+  const [isLoadingInput, setIsLoadingInput] = React.useState(false);
+  const [isLoadingCheckboxById, setIsLoadingCheckboxById] = React.useState('');
+  const [isLoadingItemById, setIsLoadingItemById] = React.useState('');
 
   React.useEffect(() => {
     pullList();
   }, []);
 
   const pullList = () => {
-    setIsLoading(true);
-    axios.get(process.env.REACT_APP_BASE_URL + '/todo-list/').then((response) => {
+    setIsLoadingTodoList(true);
+    axios.get(process.env.REACT_APP_BASE_URL + '/todo-list/').then(response => {
+      setIsLoadingTodoList(false);
       const { data } = response;
       setList(data);
-      setIsLoading(false);
     });
   };
 
   const handleEnterNewItem = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (title !== '' && event.key === 'Enter') {
-      setIsLoading(true);
-      axios.post(process.env.REACT_APP_BASE_URL + '/todo-list/', { title: title }).then((response) => {
-        pullList();
+      setIsLoadingInput(true);
+      axios.post(process.env.REACT_APP_BASE_URL + '/todo-list/', { title }).then(response => {
+        setIsLoadingInput(false);
+        const newItem: Todo = response.data;
+        setList(prevList => [...prevList, newItem]);
+        setTitle('');
       });
-      setTitle('');
     }
   };
 
   const handleDeleteItem = (id: string) => {
-    setIsLoading(true);
-    axios.delete(process.env.REACT_APP_BASE_URL + '/todo-list/' + id).then((response) => {
-      pullList();
+    setIsLoadingItemById(id);
+    axios.delete(process.env.REACT_APP_BASE_URL + '/todo-list/' + id).then(response => {
+      setIsLoadingItemById('');
+      setList(prevList => prevList.filter(item => item.id !== id));
     });
   };
 
   const toogleFinishItem = (id: string) => {
-    setIsLoading(true);
-    axios.put(process.env.REACT_APP_BASE_URL + '/todo-list/' + id).then((response) => {
-      pullList();
+    setIsLoadingCheckboxById(id);
+    axios.put(process.env.REACT_APP_BASE_URL + '/todo-list/' + id).then(response => {
+      setIsLoadingCheckboxById('');
+      setList(prevList => prevList.map(item => (item.id === id ? { ...item, isFinish: !item.isFinish } : item)));
     });
   };
 
   return (
-    <div className="app">
-      <h1 className="app__title">To-do List</h1>
-      <input
-        type="text"
-        className="app__new-item"
-        placeholder="Enter new item"
-        onKeyPress={handleEnterNewItem}
-        value={title}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTitle(event.target.value)}
-      />
-      <span>Current place is: {typeof place === 'string' ? place : place?.custom}</span>
-      <button onClick={() => setPlace('home')}>Home</button>
-      <button onClick={() => setPlace('work')}>Work</button>
-      <input
-        type="text"
-        placeholder="Enter a place"
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPlace({ custom: event.target.value })}
-      />
-      {isLoading ? <span>Loading...</span> : <span>Done</span>}
-      <div className="app__todo-list">
-        {list.map((item: Todo) => (
-          <div key={item.id} className="app__todo-item">
-            <div>
-              <input type="checkbox" onChange={() => toogleFinishItem(item.id)} />
-              <span>{item.title}</span> ||{' '}
-              <span>{typeof item.place === 'string' ? item.place : item.place?.custom}</span>
-            </div>
-            <button onClick={() => handleDeleteItem(item.id)}>x</button>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Wrapper>
+      <InputWrapper>
+        <Input
+          type="text"
+          placeholder="Enter new task"
+          value={title}
+          onChange={event => {
+            if (isLoadingInput === false) {
+              setTitle(event.currentTarget.value);
+            }
+          }}
+          onKeyPress={handleEnterNewItem}
+        />
+        {isLoadingInput ? <SpinnerInput src={asset_spinner} alt="asset_spinner" /> : null}
+      </InputWrapper>
+      <TodoList>
+        {isLoadingTodoList ? (
+          <SpinnerTodoList src={asset_spinner} alt="asset_spinner" />
+        ) : (
+          <>
+            {list.map(item => (
+              <Item key={item.id}>
+                {isLoadingCheckboxById === item.id ? (
+                  <SpinnerCheckbox type="checkbox" defaultChecked disabled />
+                ) : (
+                  <Checkbox type="checkbox" checked={item.isFinish} onChange={() => toogleFinishItem(item.id)} />
+                )}
+                <Description>{item.title}</Description>
+                {isLoadingItemById === item.id ? (
+                  <SpinnerItem src={asset_spinner} alt="asset_spinner" />
+                ) : (
+                  <DeleteButton onClick={() => handleDeleteItem(item.id)} />
+                )}
+              </Item>
+            ))}
+          </>
+        )}
+      </TodoList>
+    </Wrapper>
   );
 }
 
